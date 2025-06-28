453bce24e4b813ad2d732c2d509c3cef3d82e179
Leeban <leeban.xasan@outlook.com>
2025-06-28 14:49:03 +0100

    test dev only

--- DevOps Project/modules/database/main.tf
+++ DevOps Project/modules/database/main.tf
@@ -21,7 +21,7 @@ resource "aws_db_instance" "main" {
   allocated_storage      = 5
   engine                 = "mysql"
   engine_version         = "5.7"
-  instance_class         = "db.t2.micro"
+  instance_class         = "db.t3.micro"
   db_name                = var.db_name
   username               = var.db_username
   password               = var.db_password
